0f4e369a22098fb71b8cf01d614d7988649c64a1
archanabuilds <archana03964@gmail.com>
2026-08-09 21:23:21 +0530

    Time: 1 ms (99.97%), Space: 46.5 MB (57.51%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if(magazine.length() < ransomNote.length())
+          return false;
+        
+        int[] freq = new int[26];
+
+       for(char ch : magazine.toCharArray())
+       {
+        freq[ch - 'a']++;
+       }
+       for(char ch : ransomNote.toCharArray())
+       {
+        
+        if(freq[ch - 'a'] == 0) return false;
+
+        freq[ch - 'a']--;
+       }
+      return true;
+    }
+}
