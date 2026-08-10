a8d31a74eafd0daaea1bad1175b23813f64e09f1
archanabuilds <archana03964@gmail.com>
2026-08-10 19:36:24 +0530

    Time: 0 ms (100%), Space: 42.6 MB (87.9%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -0,0 +1,14 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        HashMap<Character,Integer> hs = new HashMap<>();
+   for(char ch : s.toCharArray())
+   {
+    hs.put(ch,hs.getOrDefault(ch,0)+1);
+    if(hs.get(ch) == 2)
+    {
+        return ch;
+    }
+   }
+   return ' ';
+ }
+}
