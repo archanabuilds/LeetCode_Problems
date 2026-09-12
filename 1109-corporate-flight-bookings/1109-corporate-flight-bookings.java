8a3d097c94941758d14b7b08a5f25f67cf701783
archanabuilds <archana03964@gmail.com>
2026-09-12 20:43:26 +0530

    Time: 3 ms (98.16%), Space: 84.5 MB (73.68%) - LeetHub

---
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+
+        int[] diff = new int[n];
+
+        for (int[] booking : bookings) {
+            int first = booking[0];
+            int last = booking[1];
+            int seats = booking[2];
+
+            diff[first - 1] += seats;
+
+            if (last < n) {
+                diff[last] -= seats;
+            }
+        }
+
+        // Prefix sum
+        for (int i = 1; i < n; i++) {
+            diff[i] += diff[i - 1];
+        }
+
+        return diff;
+    }
+}
